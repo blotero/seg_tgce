726227f596ad017c95c65efb5f47293b28c8c6ea
Brandon Lotero <blotero@gmail.com>
2024-04-29 23:41:10 -0500

    core\feat: #26 crowd seg generator
    - further definitions on crowd seg dataset generator

--- core/seg_tgce/data/crowd_seg/map.py
+++ core/seg_tgce/data/crowd_seg/map.py
@@ -18,11 +18,3 @@ def find_annotators_alias(data_target: DataTarget, base_dir_path: Path) -> list[
         path.name for path in data_target_path.iterdir() if path.is_dir()
     ]
     return annotators_alias
-
-
-def produce_tf_dataset(
-    data_target: DataTarget, base_dir_path: Path, annotator_alias: str
-) -> Path:
-    data_target_path = base_dir_path / MASKS_SUB_DIR / data_target.value
-    tf_image_folder = data_target_path / annotator_alias
-    return tf_image_folder
