825b462864213e975f74b96a794f8c0d5b53c873
Brandon Lotero <blotero@gmail.com>
2024-05-13 22:31:16 -0500

    core\feat: #45 remove non pypi deps
    - remove dependency to github package `python-gcpds.image_segmentation`
      for enabling pypi publishing
    - locally implemented oxford pet dataset mapper

--- core/seg_tgce/data/utils.py
+++ core/seg_tgce/data/utils.py
@@ -1,29 +1,29 @@
 from typing import Tuple
 
 import tensorflow as tf
-from gcpds.image_segmentation.datasets.segmentation import OxfordIiitPet
 from keras.models import Model
+from tensorflow import Tensor
 
 
 def disturb_mask(
     model: Model,
-    image: tf.Tensor,
+    image: Tensor,
     model_shape: Tuple[int, int],
     target_shape: Tuple[int, int],
-) -> tf.Tensor:
+) -> Tensor:
     return tf.image.resize(model(tf.image.resize(image, model_shape)), target_shape)
 
 
-def mix_channels(mask: tf.Tensor) -> tf.Tensor:
+def mix_channels(mask: Tensor) -> Tensor:
     return tf.stack([mask, 1 - mask], axis=-2)
 
 
 def add_noisy_annotators(
-    img: tf.Tensor,
-    models: list[tf.Tensor],
+    img: Tensor,
+    models: list[Tensor],
     model_shape: Tuple[int, int],
     target_shape: Tuple[int, int],
-) -> tf.Tensor:
+) -> Tensor:
     return tf.transpose(
         [
             disturb_mask(model, img, model_shape=model_shape, target_shape=target_shape)
@@ -34,12 +34,12 @@ def add_noisy_annotators(
 
 
 def map_dataset_multiple_annotators(
-    dataset: OxfordIiitPet,
+    dataset: Tensor,
     target_shape: tuple[int, int],
     model_shape: tuple[int, int],
     batch_size: int,
     disturbance_models: list[Model],
-) -> tf.Tensor:
+) -> Tensor:
     dataset_ = dataset.map(
         lambda img, mask, label, id_img: (img, mask),
         num_parallel_calls=tf.data.AUTOTUNE,
